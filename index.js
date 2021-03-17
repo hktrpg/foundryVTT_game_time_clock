3370dbd148ab24b33bb6729fe6579cb497642036
zetetic <>
2021-03-17 18:48:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const updateSpeed = 250;
+Hooks.once("init", () => {
+    game.settings.register("game_time_clock", "log", {
+        Time: Number
+    });
 
+    setInterval(
+        doUpdates,
+        updateSpeed
+    );
+});
+const doUpdates = () => {
+    try {
+        let time = Date.now();
+        let deltaS = time - lastSecond;
+        lastTime = time;
+        if (deltaS > 1000) {
+            lastSecond += 1000;
+            console.log(lastTime)
+            let a = game.settings.get("game_time_clock", "log")
+            game.settings.set("game_time_clock", "log", {
+                Time: lastTime
+            })
+            console.log(a)
+        }
+
+    } catch (e) {}
+};
