d455ac8f8898a22f4c5f0c4babee24ca43164c83
zetetic <>
2021-03-18 18:58:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const updateSpeed = 250;
-let lastSecond = Date.now();
-let lastTime = Date.now();
+const updateSpeed = 500;
+
+
 Hooks.once("init", () => {
+    /**
+     * 首先檢查有沒有ONLINE 的GM 和GM以外的人
+     * 
+     * 
+     * 
+     */
     game.settings.register("game_time_clock", "log", {
-        Time: Number
+        GMTime: Number,
+        GMwithPlayerTime: Number
     });
+    var playTime = game.settings.get("game_time_clock", "log");
     setInterval(
         doUpdates,
         updateSpeed
@@ -12,12 +20,11 @@ Hooks.once("init", () => {
 });
 const doUpdates = () => {
     try {
-        let a = game.settings.get("game_time_clock", "log")
-        console.log('log', a)
+
         let time = Date.now();
         let deltaS = time - lastSecond;
         lastTime = time;
-        if (true) {
+        if (deltaS > 1000) {
             lastSecond += 1000;
             console.log('lastTime', lastTime)
             game.settings.set("game_time_clock", "log", {
